84addfbc2e81f5d57eaedba38ddcd682fd15192f
Vechnuy-student <ilya_masterok@mail.ru>
2016-04-15 20:28:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Animal cat = new Animal();
+        cat.setName("I'm cat");
+
+        Animal dog = new Animal();
+
+        int i = 1;
+        long l = 2;
+
+    }
+}
